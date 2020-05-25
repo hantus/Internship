68eb40181d8b46843db4dd58154ff8fe96a65c1a
Piotr Handkowski <piotr.handkowski@etu.univ-grenoble-alpes.fr>
2020-05-25 12:15:20 +0200

    merged all data files into one

---
+++ dataSetMerger.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+dataSet1 = np.load('data/1person10_merged.npy')
+dataSet2 = np.load('data/1person_hat10_merged.npy')
+dataSet3 = np.load('data/1person_hood10_merged.npy')
+dataSet4 = np.load('data/2ppl10_merged.npy')
+dataSet5 = np.load('data/2ppl_1hat10_merged.npy')
+
+dataSets = []
+dataSets.append(dataSet1)
+dataSets.append(dataSet2)
+dataSets.append(dataSet3)
+dataSets.append(dataSet4)
+dataSets.append(dataSet5)
+
+data = []
+for ds in dataSets:
+    for i in range(ds.shape[0]):
+        data.append(ds[i])
+data = np.asarray(data)
+print(data.shape)
+
+labels1 = np.load('data/1person10_merged_Labels.npy')
+labels2 = np.load('data/1person_hat10_merged_Labels.npy')
+labels3 = np.load('data/1person_hood10_merged_Labels.npy')
+labels4 = np.load('data/2ppl10_merged_Labels.npy')
+labels5 = np.load('data/2ppl_1hat10_merged_Labels.npy')
+
+labelSets = []
+labelSets.append(labels1)
+labelSets.append(labels2)
+labelSets.append(labels3)
+labelSets.append(labels4)
+labelSets.append(labels5)
+
+labels = []
+for ds in labelSets:
+    for i in range(ds.shape[0]):
+        labels.append(ds[i])
+labels = np.asarray(labels)
+print(labels.shape)
+
+np.save('data/dataSet.npy', data)
+np.save('data/labels.npy', labels)
+
+
+
