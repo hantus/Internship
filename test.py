581df983922c744e754d2888663729ef68d25b97
Piotr Handkowski <piotr.handkowski@etu.univ-grenoble-alpes.fr>
2020-05-28 16:14:00 +0200

    added counterSideAdj

--- test.py
+++ test.py
@@ -1,35 +1,13 @@
 
 import numpy as np
 import cv2
-from joblib import  load
 
-# data = np.load('data/1person10_merged.npy')
-# model = load("data/models/model1.joblib")
-# one = 0
-# two = 0
-# three = 0
-# for i in range(data.shape[0]):
-#     frame = data[i]
-#     frame = np.reshape(frame,(1,640))
-#     pred = model.predict(frame)
-#     if pred[0] == 1:
-#         one += 1
-#     elif pred[0] == 2:
-#         two += 1
-#     elif pred[0] == 3:
-#         three += 1
+a = []
+print(a)
 
-# print("one {}, two {}, three {}".format(one, two, three))
+a.insert(2, 'R')
 
-queue = []
-
-for i in range(20):
-    if len(queue) == 10:
-        queue.pop(0)
-
-    queue.append(i)
-    print(queue)
-
+print(a.index(1))
     
 
 
